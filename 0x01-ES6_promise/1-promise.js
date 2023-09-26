8b40291849e627d57fe7659f6b64f6c397e85529
alexsameri <samerialexx@gmail.com>
2023-09-26 23:37:40 +0300

    Promise kept

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -5,9 +5,8 @@ export default function getFullResponseFromAPI(success) {
           status: 200,
           body: 'Success',
         });
-      }
-      else {
-        reject(newError('The fake API is not working currently'));
+      } else {
+        reject(new Error('The fake API is not working currently'));
       }
     });
   }
