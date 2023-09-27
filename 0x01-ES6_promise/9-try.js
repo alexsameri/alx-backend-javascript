1bbf40a3dafb285d358f8b15f947920410f9e609
alexsameri <samerialexx@gmail.com>
2023-09-28 00:52:58 +0300

    Throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -4,7 +4,7 @@ export default function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (err) {
-    queue.push('Error: cannot divide by o');
+    queue.push(String(err));
   } finally {
     queue.push('Guardrail was processed');
   }
