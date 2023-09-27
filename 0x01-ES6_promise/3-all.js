7da7320c857369451b42dcfa66a20c0c8f6bab8f
alexsameri <samerialexx@gmail.com>
2023-09-27 05:41:18 +0000

    Simple promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,10 @@
-import { uploadPhoto, createUser } from './utils.js'
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
     .then((res) => {
-      console.log(`${body} ${firstName} ${lastName}`)
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    
-    .catch((err) => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
