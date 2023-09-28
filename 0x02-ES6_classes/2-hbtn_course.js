54f6412c40a8c3b6d80779ee0a40b4c4d31958ba
alexsameri <samerialexx@gmail.com>
2023-09-29 01:12:13 +0300

    Classes

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,43 +1,52 @@
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  /**
+     * @param {String} name
+     */
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = name;
-    this._length = length;
-    this._students = students;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
-    } else {
-      throw new Error('Name must be a string');
+  /**
+     * @param {Number} length
+     */
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
     }
+    this._length = length;
   }
 
   get length() {
     return this._length;
   }
 
-  set length(newLength) {
-    if (typeof newLengthength === 'number') {
-      this._lenth = newLength;
+  /**
+     * @param {Array} students
+     */
+  set students(students) {
+    if (students instanceof Array) {
+      this._students = students;
     } else {
-      throw new Error('Length must be a number');
+      throw new TypeError('Students must be an Array');
     }
   }
 
   get students() {
     return this._students;
   }
-
-  set students(newStudents) {
-    if (Array.isArray(newStudents)) {
-      this._students = newStudents;
-    } else {
-      throw new Error('Students must be an array');
-    }
-  }
 }
+
+export default HolbertonCourse;
