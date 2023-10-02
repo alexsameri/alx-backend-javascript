964a63a5150b93027033a26fb8e5b86284539d4f
alexsameri <samerialexx@gmail.com>
2023-10-03 00:00:28 +0300

    More mapping

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,6 @@
 export default function getListStudentIds(getListStudents) {
-    if (getListStudents instanceof Array) {
-        getListStudents = getListStudents.map((students) => students.id);
-    } else {
-        return []
-    }
-
-    return getListStudents;
+  if (getListStudents instanceof Array) {
+    return getListStudents.map((students) => students.id);
+  }
+  return [];
 }
